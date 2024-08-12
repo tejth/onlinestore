bda073684917e626ba67279a11fe652a4dcac002
tejth <ptejendra91@gmail.com>
2024-08-13 03:52:11 +0530

    set tailwindcss

---
+++ onlinestore/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta
+          name="description"
+          content="tusharelectricals.com - Buy electricals Items"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-slate-600">hey i am tej</div>
+    </>
+  );
+}
